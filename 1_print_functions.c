e07e008c746c0a3a5324e128c36580a7706496fa
maroua199525 <2646@holbertonschool.com>
2021-03-16 18:03:18 +0000

    print functions conversion

---
+++ 1_print_functions.c
@@ -0,0 +1,41 @@
+#include "holberton.h"
+/**
+ * print_character - print a char
+ * @arg: argument of type va_list
+ * Return: always 1
+ */
+int print_character(va_list arg)
+{
+	char ch;
+
+	ch = va_arg(arg, int);
+	_putchar(ch);
+	return (1);
+}
+/**
+ *print_string - print a string
+ *@arg: argument of type va_list
+ * Return: len
+ */
+int print_string(va_list arg)
+{
+	int len = 0;
+	char *str;
+
+	str = va_arg(arg, char*);
+	if (str == NULL)
+		str = "(null)";
+	while (str[len] != '\0')
+	{
+		_putchar(str[len]);
+		len++;
+	}
+	return (len);
+}
+int print_percent(va_list arg)
+{
+	(void)arg;
+
+	_putchar('%');
+	return (1);
+}
